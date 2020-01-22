ee8b61322bcff6f07d6ee8d64e3f55d36bd04134
Tom Pound <tompound@ymail.com>
2020-01-22 00:21:44 +0000

    Major Updates 22/1/20

--- src/main/EngineeringApplication.java
+++ src/main/EngineeringApplication.java
@@ -1,9 +1,14 @@
 package main;
 
 
+import java.io.File;
+
 import com.interactivemesh.jfx.importer.stl.StlMeshImporter;
 import com.interactivemesh.jfx.importer.tds.TdsModelImporter;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -21,8 +26,11 @@ import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.MeshView;
 import javafx.scene.shape.TriangleMesh;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class EngineeringApplication extends Application{
 
@@ -38,6 +46,9 @@ public class EngineeringApplication extends Application{
 	private Scene scene1;
 	private Scene scene2;
 	private Stage stage;
+	private Timeline timeline;
+	private int animationDuration = 15;
+	private boolean mainModelPaused = false;
 	
 	public static void main(String[] args) {
 		System.out.println("Running...");
@@ -57,10 +68,12 @@ public class EngineeringApplication extends Application{
 		//Setting up Scenes
 		setupHome();
 		setupMain();
+		setupSettings();
 		System.out.println("Scenes Setup...");
 		
 		stage.setScene(home);
 		stage.show();
+		
 	}
 	
 	private void setupHome() {
@@ -91,6 +104,7 @@ public class EngineeringApplication extends Application{
 		//Finalise Scene
 		home = new Scene(gridPane, width, height);
 		home.getStylesheets().add("style/homeScreen.css");
+		System.out.println("Home Screen Setup...");
 		
 	}
 	private void setupMain() {
@@ -103,6 +117,7 @@ public class EngineeringApplication extends Application{
 		
 		//Set title
 		Text title = new Text("3D Model!");
+		title.setTextAlignment(TextAlignment.CENTER);
 		//Setup Buttons
 		Button mainButton = new Button("Learn!");
 		mainButton.setOnAction(e -> mainButtonPress());
@@ -113,45 +128,109 @@ public class EngineeringApplication extends Application{
 		Button quitButton = new Button("Quit");
 		quitButton.setOnAction(e -> quitButtonPress());
 		
-		//Import 3D model
-		//3DS
-		TdsModelImporter tdsImporter = new TdsModelImporter();
-		tdsImporter.read("src/3D_Models/HST-3DS/hst.3ds");
-		Node[] tdsMesh = (Node[]) tdsImporter.getImport();
-		tdsImporter.close();
-		
-		//STL
-		StlMeshImporter stlImporter = new StlMeshImporter();
-		stlImporter.read("src/3D_Models/");
-		TriangleMesh stlMesh = stlImporter.getImport();
-		
-		MeshView stlMeshView = new MeshView();
-		stlMeshView.setMaterial(new PhongMaterial(Color.GRAY));
-		stlMeshView.setMesh(stlMesh);
-		stlImporter.close();
-		
-		//Add Elements
 		Camera camera = new PerspectiveCamera();
 		Group model = new Group();
 		
-		model.getChildren().addAll(tdsMesh);
-		model.getChildren().add(stlMeshView);
+		//Import 3D model
+		
+		//File modelUrl = new File("src/3D_Models/HST-3DS/hst.3ds");
+		String fileExtention = "3ds";
+		
+		if(fileExtention == "3ds") {
+			//3DS
+			TdsModelImporter tdsImporter = new TdsModelImporter();
+			tdsImporter.read("3D_Models/HST-3DS/hst.3ds");
+			Node[] tdsMesh = (Node[]) tdsImporter.getImport();
+			tdsImporter.close();
+			model.getChildren().addAll(tdsMesh);
+		}else if(fileExtention == "stl") {
+			//STL
+			StlMeshImporter stlImporter = new StlMeshImporter();
+			stlImporter.read("src/3D_Models/");
+			TriangleMesh stlMesh = stlImporter.getImport();
+			
+			MeshView stlMeshView = new MeshView();
+			stlMeshView.setMaterial(new PhongMaterial(Color.GRAY));
+			stlMeshView.setMesh(stlMesh);
+			stlImporter.close();
+			model.getChildren().add(stlMeshView);
+		}else {
+			System.out.println("Model file type not supported/File not found");
+		}
+		
+		//Add Elements
 		model.getTransforms().add(new Rotate(90, Rotate.X_AXIS));
 		
+		Translate pivot =  new Translate();
+		Rotate yRotate = new Rotate(0, Rotate.Y_AXIS);
+		
+		camera.getTransforms().addAll(pivot, yRotate, new Rotate(-20, Rotate.X_AXIS));
+		camera.getTransforms().add(new Translate(-340,-200,-300));
+		
+		//Set Camera Animation
+        timeline = new Timeline(
+                new KeyFrame(
+                        Duration.seconds(0), 
+                        new KeyValue(yRotate.angleProperty(), 0)
+                ),
+                new KeyFrame(
+                        Duration.seconds(15), 
+                        new KeyValue(yRotate.angleProperty(), 360)
+                )
+        );
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+		
 		//Add to pane
-		gridPane.add(title,1,0);
+		gridPane.add(title,3,0);
 		gridPane.add(mainButton,4,0);
 		gridPane.add(settingsButton,5,0);
 		gridPane.add(quitButton,6,0);
 		
 		SubScene modelScene = new SubScene(model, 680, 400, true, SceneAntialiasing.BALANCED);
-		
+		modelScene.setCamera(camera);
+		modelScene.setOnMouseEntered(e -> mouseOnModel());
+		modelScene.setOnMouseExited(e -> mouseOffModel());
+		modelScene.setOnMousePressed(e -> checkMouseCoords(e.getX(), e.getY()));
 		gridPane.add(modelScene, 3, 1);
+		
+		//gridPane.setRowSpan(title, 3);
+		//gridPane.setColumnSpan(modelScene, 1);
+		
 		//Finalise Scene
 		main = new Scene(gridPane, width, height);
 		main.getStylesheets().add("style/mainScreen.css");
+		System.out.println("Main Screen Setup...");
 	}
 	
+	private void setupSettings() {
+		//Scene Setup
+		GridPane gridPane = new GridPane();
+		gridPane.setPadding(new Insets(10,10,10,10));
+		gridPane.setVgap(10);
+		gridPane.setHgap(10);
+		gridPane.setAlignment(Pos.CENTER);
+		
+		//Set title
+		Text title = new Text("Settings!");
+		title.setTextAlignment(TextAlignment.CENTER);
+		//Setup Buttons
+		Button homeButton = new Button("Back!");
+		homeButton.setOnAction(e -> homeButtonPress());
+
+		gridPane.add(title, 1, 0);
+		gridPane.add(homeButton, 1, 1);
+		settings = new Scene(gridPane, width, height);
+		settings.getStylesheets().add("style/settingsScreen.css");
+		System.out.println("Settings Screen Setup...");
+	}
+	
+	private void setupSlide() {
+		
+	}
+	private void homeButtonPress() {
+		stage.setScene(home);
+	}
 	private void mainButtonPress() {
 		stage.setScene(main);
 	}
@@ -160,6 +239,23 @@ public class EngineeringApplication extends Application{
 	}
 	private void quitButtonPress() {
 		System.exit(1);
+	}
+	private void mouseOnModel() {
+		timeline.pause();
+		System.out.println(timeline.getCurrentTime());
+		double currentTime = timeline.getCurrentTime().toSeconds();;
+		double angleRotated = currentTime/animationDuration * 360;
+		System.out.println((int)angleRotated + " degrees");
+		mainModelPaused = true;
+	}
+	private void mouseOffModel() {
+		timeline.play();;
+		System.out.println("Model Resumed");
+		mainModelPaused = false;
+	}
+	
+	private void checkMouseCoords(double mouseX, double mouseY) {
+		
 	}
 
 }
